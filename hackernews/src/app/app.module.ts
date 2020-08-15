75b8e0a189b6b24da47d598b031c296853bd933c
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2020-08-15 11:54:12 +0530

    first commit

---
+++ hackernews/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ hackernews/src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer} from '@angular/platform-browser';
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+  allArticles: Array<any> = [];
+  articles: Array<any> = [];
+  bookmarks = 0;
+  showBookMarks = false;
+  webpage: any;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.http.get('./../assets/json/feed.json').subscribe((response: any) => { 
+      this.allArticles = response.articles;
+      this.articles = this.allArticles.filter(c => c);
+     });
+  }
+
+  bookmarkMe(article) {
+    article.bookmarked = !article.bookmarked;
+    this.updateBookmarks();
+  }
+
+  updateBookmarks() {
+    const filtered = this.allArticles.filter(c => c.bookmarked);
+    this.bookmarks = filtered.length;
+  }
+
+  toggleBookMarks() {
+    if(!this.showBookMarks) {
+      this.articles = this.allArticles.filter(c => c.bookmarked);
+      this.showBookMarks = true;
+    } else {
+      this.articles = this.allArticles.filter(c => c);
+      this.showBookMarks = false;
+    }
+  }
+  openWebpage(url){
+    this.webpage = this.sanitizer.bypassSecurityTrustResourceUrl(url); 
+  }
+  closeWebpage() {
+    this.webpage = '';
+  }
+
+  toggleComments(article) {
+    article.start = 0;
+    article.end = 2;
+  }
+
+}
